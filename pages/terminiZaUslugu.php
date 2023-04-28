a0694b578611b70384dd85304ffd627535aa9c39
RadivojkaObucina <76410434+RadivojkaObucina@users.noreply.github.com>
2023-04-28 17:45:43 +0200

    Update terminiZaUslugu.php

--- pages/terminiZaUslugu.php
+++ pages/terminiZaUslugu.php
@@ -104,6 +104,8 @@
     </div>
 
   </div>
+
+
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
@@ -183,6 +185,7 @@
     })
 
 
+
     function vratiTermine() {
       $.getJSON('../terminHandlers/getAllByUsluga.php', { id: usluga.id }, function (data) {
         if (data.status != 1) {
@@ -199,6 +202,7 @@
         napuniTabelu();
       })
     }
+    
 
     function napuniTabelu() {
       $('#termini').html('');
